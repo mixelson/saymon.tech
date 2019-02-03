2464cc7d08979758a9904df8413ff72c13502a22
KM <mixelson@gmail.com>
2019-02-03 11:58:28 +0100

    style and connect

--- files/faces/list.sh
+++ files/faces/list.sh
@@ -1,1 +1,1 @@
-ls -1 | perl -e '{while(<>){next unless /jpg/; next if /KM2/; chomp; print "<img src=\"files/faces/$_\" style=\"width:100px;\" >\n"}}'
+ls -1 | perl -e '{while(<>){next unless /jpg/; next if /KM2/; chomp; print "<img src=\"files/faces/$_\" style=\"width:100px; margin-top: 3px;\" >\n"}}'
